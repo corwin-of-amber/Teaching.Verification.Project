684b7bb158a0fd10ed48942da2734625bb6d3013
Shachar Itzhaky <corwin.amber@gmail.com>
2021-05-22 17:44:40 +0300

    [feature] Reference UI - basic scaffolding.

---
+++ ref-src/ui/src/index.ts
@@ -0,0 +1,9 @@
+import CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+
+function main() {
+    CodeMirror(document.body);
+}
+
+
+document.addEventListener('DOMContentLoaded', main);
---
+++ ref-src/ui/webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack'),
+      path = require('path');
+
+module.exports = (env, argv) => [{
+  name: 'ide-project',
+  mode: argv.mode || 'development',
+  entry: './index.ts',
+  devtool: argv.mode !== 'production' ? "source-map" : undefined,
+  stats: {
+    hash: false, version: false, modules: false  // reduce verbosity
+  },
+  performance: {
+    maxAssetSize: 1e6, maxEntrypointSize: 1e6   // 250k is too small
+  },
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'static'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/i,  /* Vue.js has some */
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'ide-project-images',
+        }
+      }
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  }
+}];
