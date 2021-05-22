017655d8b0cc4a50f973e4c4046de87d4ab0a324
Shachar Itzhaky <corwin.amber@gmail.com>
2021-05-22 18:19:39 +0300

    [feature] Added AJAX for list and verify.

--- ref-src/ui/app.py
+++ ref-src/ui/app.py
@@ -1,3 +1,5 @@
+import subprocess
+import json
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return open('index.html').read()
+
+@app.route("/benchmarks")
+def list_benchmarks():
+    return json.dumps(['list your', 'benchmakrs', 'here'])
+
+@app.route("/verify")
+def verify():
+    p = subprocess.run("ls", stdout=subprocess.PIPE)
+    return p.stdout
--- ref-src/ui/src/index.ts
+++ ref-src/ui/src/index.ts
@@ -21,17 +21,37 @@ class IDE {
         this.cm = CodeMirror(this.panels.editor, {
             lineNumbers: true
         });
+
+        this.panels.output.querySelector('button')
+            .addEventListener('click', () => this.runVerifier());
     }
 
     async open(uri: string) {
-        var doc = new CodeMirror.Doc(await (await fetch(uri)).text());
+        var text = await (await fetch(uri)).text(),
+            doc = new CodeMirror.Doc(text);
         this.cm.swapDoc(doc);
+    }
+
+    async runVerifier() {
+        var out = await (await fetch('/verify')).text();
+        this.panels.output.querySelector('#output').textContent = out;
+    }
+
+    async listBenchmarks() {
+        var ul = this.panels.benchmarks.querySelector('#benchmarks'),
+            items = await (await fetch('/benchmarks')).json();
+        for (let item of items) {
+            var li = document.createElement('li');
+            li.textContent = item;
+            ul.append(li);
+        }
     }
 }
 
 
 function main() {
     var ide = new IDE;
+    ide.listBenchmarks();
     ide.cm.focus();
 
     ide.open('/');
